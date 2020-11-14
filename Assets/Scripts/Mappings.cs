b21c1bd714acbdc2b6053ff9165dde3a035d241f
David Briscoe <idbrii@gmail.com>
2020-11-13 17:20:08 -0800

    Align fields in mappings

--- Assets/Scripts/Mappings.cs
+++ Assets/Scripts/Mappings.cs
@@ -99,28 +99,28 @@ namespace idbrii.InputValidation
 
                 m_Buttons = new List<Button>
                 {
-                    new Button{name="Button A", key=KeyCode.JoystickButton1}, 
-                    new Button{name="Button B", key=KeyCode.JoystickButton2}, 
-                    new Button{name="Button X", key=KeyCode.JoystickButton4}, 
-                    new Button{name="Button Y", key=KeyCode.JoystickButton5}, 
-                    new Button{name="Left Bumper", key=KeyCode.JoystickButton7}, 
-                    new Button{name="Right Bumper", key=KeyCode.JoystickButton8}, 
-                    new Button{name="Back", key=KeyCode.JoystickButton16}, 
-                    new Button{name="Start", key=KeyCode.JoystickButton12}, 
-                    new Button{name="Left Stick Button", key=KeyCode.JoystickButton14}, 
-                    new Button{name="Right Stick Button", key=KeyCode.JoystickButton15},
-                },
+                    new Button{ name="Button A",           key=KeyCode.JoystickButton1 },
+                    new Button{ name="Button B",           key=KeyCode.JoystickButton2 },
+                    new Button{ name="Button X",           key=KeyCode.JoystickButton4 },
+                    new Button{ name="Button Y",           key=KeyCode.JoystickButton5 },
+                    new Button{ name="Left Bumper",        key=KeyCode.JoystickButton7 },
+                    new Button{ name="Right Bumper",       key=KeyCode.JoystickButton8 },
+                    new Button{ name="Back",               key=KeyCode.JoystickButton16 },
+                    new Button{ name="Start",              key=KeyCode.JoystickButton12 },
+                    new Button{ name="Left Stick Button",  key=KeyCode.JoystickButton14 },
+                    new Button{ name="Right Stick Button", key=KeyCode.JoystickButton15 },
+                 },
 
                 m_Axes = new List<Axis>
                 {
-                    new Axis{name="Left Stick Vertical", id=1},
-                    new Axis{name="Left Stick Horizontal", id=0},
-                    new Axis{name="Right Stick Vertical", id=3},
-                    new Axis{name="Right Stick Horizontal", id=2},
-                    new Axis{name="Left Trigger", id=4},
-                    new Axis{name="Right Trigger", id=5},
-                    new Axis{name="DPAD Vertical", id=-1 }, // "dpad is 5, but 5 often gets stuck at -1."
-                    new Axis{name="DPAD Horizontal", id=-1 }, // "dpad is 4, but we can't use DPAD Vertical."
+                    new Axis{ name="Left Stick Vertical",    id=1 },
+                    new Axis{ name="Left Stick Horizontal",  id=0 },
+                    new Axis{ name="Right Stick Vertical",   id=3 },
+                    new Axis{ name="Right Stick Horizontal", id=2 },
+                    new Axis{ name="Left Trigger",           id=4 },
+                    new Axis{ name="Right Trigger",          id=5 },
+                    new Axis{ name="DPAD Vertical",          id=-1  }, // "dpad is 5, but 5 often gets stuck at -1."
+                    new Axis{ name="DPAD Horizontal",        id=-1  }, // "dpad is 4, but we can't use DPAD Vertical."
                 }
             };
         }
@@ -138,28 +138,28 @@ namespace idbrii.InputValidation
 
                 m_Buttons = new List<Button>
                 {
-                    new Button{name="Button A", key=KeyCode.JoystickButton0}, 
-                    new Button{name="Button B", key=KeyCode.JoystickButton1}, 
-                    new Button{name="Button X", key=KeyCode.JoystickButton2}, 
-                    new Button{name="Button Y", key=KeyCode.JoystickButton3}, 
-                    new Button{name="Left Bumper", key=KeyCode.JoystickButton4}, 
-                    new Button{name="Right Bumper", key=KeyCode.JoystickButton5}, 
-                    new Button{name="Back", key=KeyCode.JoystickButton6}, 
-                    new Button{name="Start", key=KeyCode.JoystickButton7}, 
-                    new Button{name="Left Stick Button", key=KeyCode.JoystickButton8}, 
-                    new Button{name="Right Stick Button", key=KeyCode.JoystickButton9},
-                },
+                    new Button{ name="Button A",           key=KeyCode.JoystickButton0 },
+                    new Button{ name="Button B",           key=KeyCode.JoystickButton1 },
+                    new Button{ name="Button X",           key=KeyCode.JoystickButton2 },
+                    new Button{ name="Button Y",           key=KeyCode.JoystickButton3 },
+                    new Button{ name="Left Bumper",        key=KeyCode.JoystickButton4 },
+                    new Button{ name="Right Bumper",       key=KeyCode.JoystickButton5 },
+                    new Button{ name="Back",               key=KeyCode.JoystickButton6 },
+                    new Button{ name="Start",              key=KeyCode.JoystickButton7 },
+                    new Button{ name="Left Stick Button",  key=KeyCode.JoystickButton8 },
+                    new Button{ name="Right Stick Button", key=KeyCode.JoystickButton9 },
+                 },
 
                 m_Axes = new List<Axis>
                 {
-                    new Axis{name="Left Stick Vertical", id=1},
-                    new Axis{name="Left Stick Horizontal", id=0},
-                    new Axis{name="Right Stick Vertical", id=4},
-                    new Axis{name="Right Stick Horizontal", id=3},
-                    new Axis{name="Left Trigger", id=8},
-                    new Axis{name="Right Trigger", id=9},
-                    new Axis{name="DPAD Vertical", id=6},
-                    new Axis{name="DPAD Horizontal", id=5},
+                    new Axis{ name="Left Stick Vertical",    id=1 },
+                    new Axis{ name="Left Stick Horizontal",  id=0 },
+                    new Axis{ name="Right Stick Vertical",   id=4 },
+                    new Axis{ name="Right Stick Horizontal", id=3 },
+                    new Axis{ name="Left Trigger",           id=8 },
+                    new Axis{ name="Right Trigger",          id=9 },
+                    new Axis{ name="DPAD Vertical",          id=6 },
+                    new Axis{ name="DPAD Horizontal",        id=5 },
                 }
             };
         }
@@ -176,28 +176,28 @@ namespace idbrii.InputValidation
 
                 m_Buttons = new List<Button>
                 {
-                    new Button{name="Button A", key=KeyCode.JoystickButton1}, 
-                    new Button{name="Button B", key=KeyCode.JoystickButton2}, 
-                    new Button{name="Button X", key=KeyCode.JoystickButton0}, 
-                    new Button{name="Button Y", key=KeyCode.JoystickButton3}, 
-                    new Button{name="Left Bumper", key=KeyCode.JoystickButton4}, 
-                    new Button{name="Right Bumper", key=KeyCode.JoystickButton5}, 
-                    new Button{name="Back", key=KeyCode.JoystickButton8}, 
-                    new Button{name="Start", key=KeyCode.JoystickButton9}, 
-                    new Button{name="Left Stick Button", key=KeyCode.JoystickButton10}, 
-                    new Button{name="Right Stick Button", key=KeyCode.JoystickButton11},
-                },
+                    new Button{ name="Button A",           key=KeyCode.JoystickButton1 },
+                    new Button{ name="Button B",           key=KeyCode.JoystickButton2 },
+                    new Button{ name="Button X",           key=KeyCode.JoystickButton0 },
+                    new Button{ name="Button Y",           key=KeyCode.JoystickButton3 },
+                    new Button{ name="Left Bumper",        key=KeyCode.JoystickButton4 },
+                    new Button{ name="Right Bumper",       key=KeyCode.JoystickButton5 },
+                    new Button{ name="Back",               key=KeyCode.JoystickButton8 },
+                    new Button{ name="Start",              key=KeyCode.JoystickButton9 },
+                    new Button{ name="Left Stick Button",  key=KeyCode.JoystickButton10 },
+                    new Button{ name="Right Stick Button", key=KeyCode.JoystickButton11 },
+                 },
 
                 m_Axes = new List<Axis>
                 {
-                    new Axis{name="Left Stick Vertical", id=1},
-                    new Axis{name="Left Stick Horizontal", id=0},
-                    new Axis{name="Right Stick Vertical", id=3},
-                    new Axis{name="Right Stick Horizontal", id=2},
-                    new Axis{name="Left Trigger", id=4},
-                    new Axis{name="Right Trigger", id=5},
-                    new Axis{name="DPAD Vertical", id=11 }, // "Bluetooth uses 11, but wired uses 7."
-                    new Axis{name="DPAD Horizontal", id=10 }, // "Bluetooth uses 10, but wired uses 6."
+                    new Axis{ name="Left Stick Vertical",    id=1 },
+                    new Axis{ name="Left Stick Horizontal",  id=0 },
+                    new Axis{ name="Right Stick Vertical",   id=3 },
+                    new Axis{ name="Right Stick Horizontal", id=2 },
+                    new Axis{ name="Left Trigger",           id=4 },
+                    new Axis{ name="Right Trigger",          id=5 },
+                    new Axis{ name="DPAD Vertical",          id=11  }, // "Bluetooth uses 11, but wired uses 7."
+                    new Axis{ name="DPAD Horizontal",        id=10  }, // "Bluetooth uses 10, but wired uses 6."
                 }
             };
         }
@@ -216,30 +216,30 @@ namespace idbrii.InputValidation
 
                 m_Buttons = new List<Button>
                 {
-                    new Button{name="Button A", key=KeyCode.JoystickButton14}, 
-                    new Button{name="Button B", key=KeyCode.JoystickButton13}, 
-                    new Button{name="Button X", key=KeyCode.JoystickButton15}, 
-                    new Button{name="Button Y", key=KeyCode.JoystickButton12}, 
-                    new Button{name="Left Bumper", key=KeyCode.JoystickButton8}, 
-                    new Button{name="Right Bumper", key=KeyCode.JoystickButton9}, 
-                    new Button{name="Back", key=KeyCode.None  }, // "XboxOne starts screen recording, but game gets nothing."
-                    new Button{name="Start", key=KeyCode.JoystickButton0}, 
-                    new Button{name="Left Stick Button", key=KeyCode.JoystickButton17}, 
-                    new Button{name="Right Stick Button", key=KeyCode.JoystickButton18}, 
-                    new Button{name="DPAD Up", key=KeyCode.JoystickButton4}, 
-                    new Button{name="DPAD Down", key=KeyCode.JoystickButton6}, 
-                    new Button{name="DPAD Left", key=KeyCode.JoystickButton7}, 
-                    new Button{name="DPAD Right", key=KeyCode.JoystickButton5}, 
-                    new Button{name="Left Trigger", key=KeyCode.JoystickButton10}, 
-                    new Button{name="Right Trigger", key=KeyCode.JoystickButton11},
-                },
+                    new Button{ name="Button A",           key=KeyCode.JoystickButton14 },
+                    new Button{ name="Button B",           key=KeyCode.JoystickButton13 },
+                    new Button{ name="Button X",           key=KeyCode.JoystickButton15 },
+                    new Button{ name="Button Y",           key=KeyCode.JoystickButton12 },
+                    new Button{ name="Left Bumper",        key=KeyCode.JoystickButton8 },
+                    new Button{ name="Right Bumper",       key=KeyCode.JoystickButton9 },
+                    new Button{ name="Back",               key=KeyCode.None   }, // "XboxOne starts screen recording, but game gets nothing."
+                    new Button{ name="Start",              key=KeyCode.JoystickButton0 },
+                    new Button{ name="Left Stick Button",  key=KeyCode.JoystickButton17 },
+                    new Button{ name="Right Stick Button", key=KeyCode.JoystickButton18 },
+                    new Button{ name="DPAD Up",            key=KeyCode.JoystickButton4 },
+                    new Button{ name="DPAD Down",          key=KeyCode.JoystickButton6 },
+                    new Button{ name="DPAD Left",          key=KeyCode.JoystickButton7 },
+                    new Button{ name="DPAD Right",         key=KeyCode.JoystickButton5 },
+                    new Button{ name="Left Trigger",       key=KeyCode.JoystickButton10 },
+                    new Button{ name="Right Trigger",      key=KeyCode.JoystickButton11 },
+                 },
 
                 m_Axes = new List<Axis>
                 {
-                    new Axis{name="Left Stick Vertical", id=1},
-                    new Axis{name="Left Stick Horizontal", id=0},
-                    new Axis{name="Right Stick Vertical", id=3},
-                    new Axis{name="Right Stick Horizontal", id=2},
+                    new Axis{ name="Left Stick Vertical",    id=1 },
+                    new Axis{ name="Left Stick Horizontal",  id=0 },
+                    new Axis{ name="Right Stick Vertical",   id=3 },
+                    new Axis{ name="Right Stick Horizontal", id=2 },
                 }
             };
         }
@@ -250,28 +250,28 @@ namespace idbrii.InputValidation
 
                 m_Buttons = new List<Button>
                 {
-                    new Button{name="Button A", key=KeyCode.JoystickButton0}, 
-                    new Button{name="Button B", key=KeyCode.JoystickButton1}, 
-                    new Button{name="Button X", key=KeyCode.JoystickButton2}, 
-                    new Button{name="Button Y", key=KeyCode.JoystickButton3}, 
-                    new Button{name="Left Bumper", key=KeyCode.JoystickButton4}, 
-                    new Button{name="Right Bumper", key=KeyCode.JoystickButton5}, 
-                    new Button{name="Back", key=KeyCode.JoystickButton6}, 
-                    new Button{name="Start", key=KeyCode.JoystickButton7}, 
-                    new Button{name="Left Stick Button", key=KeyCode.JoystickButton9}, 
-                    new Button{name="Right Stick Button", key=KeyCode.JoystickButton10},
-                },
+                    new Button{ name="Button A",           key=KeyCode.JoystickButton0 },
+                    new Button{ name="Button B",           key=KeyCode.JoystickButton1 },
+                    new Button{ name="Button X",           key=KeyCode.JoystickButton2 },
+                    new Button{ name="Button Y",           key=KeyCode.JoystickButton3 },
+                    new Button{ name="Left Bumper",        key=KeyCode.JoystickButton4 },
+                    new Button{ name="Right Bumper",       key=KeyCode.JoystickButton5 },
+                    new Button{ name="Back",               key=KeyCode.JoystickButton6 },
+                    new Button{ name="Start",              key=KeyCode.JoystickButton7 },
+                    new Button{ name="Left Stick Button",  key=KeyCode.JoystickButton9 },
+                    new Button{ name="Right Stick Button", key=KeyCode.JoystickButton10 },
+                 },
 
                 m_Axes = new List<Axis>
                 {
-                    new Axis{name="Left Stick Vertical", id=1},
-                    new Axis{name="Left Stick Horizontal", id=0},
-                    new Axis{name="Right Stick Vertical", id=4},
-                    new Axis{name="Right Stick Horizontal", id=3},
-                    new Axis{name="Left Trigger", id=2},
-                    new Axis{name="Right Trigger", id=5},
-                    new Axis{name="DPAD Vertical", id=7},
-                    new Axis{name="DPAD Horizontal", id=6},
+                    new Axis{ name="Left Stick Vertical",    id=1 },
+                    new Axis{ name="Left Stick Horizontal",  id=0 },
+                    new Axis{ name="Right Stick Vertical",   id=4 },
+                    new Axis{ name="Right Stick Horizontal", id=3 },
+                    new Axis{ name="Left Trigger",           id=2 },
+                    new Axis{ name="Right Trigger",          id=5 },
+                    new Axis{ name="DPAD Vertical",          id=7 },
+                    new Axis{ name="DPAD Horizontal",        id=6 },
                 }
             };
         }
@@ -286,30 +286,30 @@ namespace idbrii.InputValidation
 
                 m_Buttons = new List<Button>
                 {
-                    new Button{name="Button A", key=KeyCode.JoystickButton0}, 
-                    new Button{name="Button B", key=KeyCode.JoystickButton1}, 
-                    new Button{name="Button X", key=KeyCode.JoystickButton2}, 
-                    new Button{name="Button Y", key=KeyCode.JoystickButton3}, 
-                    new Button{name="Left Bumper", key=KeyCode.JoystickButton4}, 
-                    new Button{name="Right Bumper", key=KeyCode.JoystickButton5}, 
-                    new Button{name="Back", key=KeyCode.JoystickButton6}, 
-                    new Button{name="Start", key=KeyCode.JoystickButton7}, 
-                    new Button{name="Left Stick Button", key=KeyCode.JoystickButton9}, 
-                    new Button{name="Right Stick Button", key=KeyCode.JoystickButton10}, 
-                    new Button{name="DPAD Up", key=KeyCode.JoystickButton13}, 
-                    new Button{name="DPAD Down", key=KeyCode.JoystickButton14}, 
-                    new Button{name="DPAD Left", key=KeyCode.JoystickButton11}, 
-                    new Button{name="DPAD Right", key=KeyCode.JoystickButton12},
-                },
+                    new Button{ name="Button A",           key=KeyCode.JoystickButton0 },
+                    new Button{ name="Button B",           key=KeyCode.JoystickButton1 },
+                    new Button{ name="Button X",           key=KeyCode.JoystickButton2 },
+                    new Button{ name="Button Y",           key=KeyCode.JoystickButton3 },
+                    new Button{ name="Left Bumper",        key=KeyCode.JoystickButton4 },
+                    new Button{ name="Right Bumper",       key=KeyCode.JoystickButton5 },
+                    new Button{ name="Back",               key=KeyCode.JoystickButton6 },
+                    new Button{ name="Start",              key=KeyCode.JoystickButton7 },
+                    new Button{ name="Left Stick Button",  key=KeyCode.JoystickButton9 },
+                    new Button{ name="Right Stick Button", key=KeyCode.JoystickButton10 },
+                    new Button{ name="DPAD Up",            key=KeyCode.JoystickButton13 },
+                    new Button{ name="DPAD Down",          key=KeyCode.JoystickButton14 },
+                    new Button{ name="DPAD Left",          key=KeyCode.JoystickButton11 },
+                    new Button{ name="DPAD Right",         key=KeyCode.JoystickButton12 },
+                 },
 
                 m_Axes = new List<Axis>
                 {
-                    new Axis{name="Left Stick Vertical", id=1},
-                    new Axis{name="Left Stick Horizontal", id=0},
-                    new Axis{name="Right Stick Vertical", id=4},
-                    new Axis{name="Right Stick Horizontal", id=3},
-                    new Axis{name="Left Trigger", id=2},
-                    new Axis{name="Right Trigger", id=5},
+                    new Axis{ name="Left Stick Vertical",    id=1 },
+                    new Axis{ name="Left Stick Horizontal",  id=0 },
+                    new Axis{ name="Right Stick Vertical",   id=4 },
+                    new Axis{ name="Right Stick Horizontal", id=3 },
+                    new Axis{ name="Left Trigger",           id=2 },
+                    new Axis{ name="Right Trigger",          id=5 },
                 }
             };
         }
